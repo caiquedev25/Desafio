f6fe6fc94976ceb34f66cee4425eb29bb4152212
caiquedev25 <70437055+caiquedev25@users.noreply.github.com>
2021-11-07 13:46:01 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,11 @@
 
 use Illuminate\Http\Request;
 
+Route::get('users', 'ApiController@getAllUser');//retorno
+Route::get('users/{id}', 'ApiController@getUser');//retorna alumo especifico
 
-Route::get('students', 'ApiController@getAllUser');//retorno
-Route::get('students/{id}', 'ApiController@getUser');//retorna alumo especifico
+Route::post('users','ApiController@createUser');//create
 
-Route::post('students','ApiController@createUser');//create
+Route::put('users/{id}', 'ApiController@updateUser');
 
-Route::put('students/{id}', 'ApiController@updateUser');
-
-Route::delete('students/{id}','ApiController@deleteUser');
+Route::delete('users/{id}','ApiController@deleteUser');
